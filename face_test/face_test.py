b76c62427c6ad39015a24301adb7702b71c8787a
Tuan0324 <jhmh0226@gmail.com>
2021-05-08 22:47:21 +0900

    0508

--- face_test/face_test.py
+++ face_test/face_test.py
@@ -60,6 +60,7 @@ with mp_face_mesh.FaceMesh(
             connections=mp_face_mesh.FACE_CONNECTIONS,
             landmark_drawing_spec=drawing_spec,
             connection_drawing_spec=drawing_spec)
+        print(face_landmarks)
     cv2.imshow('MediaPipe FaceMesh', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
